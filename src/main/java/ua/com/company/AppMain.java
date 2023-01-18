5516e6b5a02be5d2ef7dedfe35411d59673b051a
Bohdan Shevyrov <bshevyrov@gmail.com>
2023-01-18 14:10:17 +0200

    add logging

--- src/main/java/ua/com/company/AppMain.java
+++ src/main/java/ua/com/company/AppMain.java
@@ -1,15 +1,19 @@
 package ua.com.company;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 import ua.com.company.entity.MyTelegramBot;
 
 public class AppMain {
+    static Logger log = LogManager.getLogger(AppMain.class);
     public static void main(String[] args) {
         try {
             TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
             botsApi.registerBot(new MyTelegramBot());
+            log.info("Connected");
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
