6c5db014af05e493d3da713e703e13f94df1214e
Bohdan Shevyrov <bshevyrov@gmail.com>
2023-01-18 21:24:10 +0200

    Get connection info

--- src/main/java/ua/com/company/AppMain.java
+++ src/main/java/ua/com/company/AppMain.java
@@ -6,13 +6,14 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 import ua.com.company.entity.MyTelegramBot;
+import ua.com.company.utils.property.TelegramPropertiesReader;
 
 public class AppMain {
     static Logger log = LogManager.getLogger(AppMain.class);
     public static void main(String[] args) {
         try {
             TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
-            botsApi.registerBot(new MyTelegramBot());
+            botsApi.registerBot(new MyTelegramBot(new TelegramPropertiesReader("src/main/resources/telegram.properties")));
             log.info("Connected");
         } catch (TelegramApiException e) {
             e.printStackTrace();
---
+++ src/main/java/ua/com/company/entity/MyIvideonEntity.java
@@ -0,0 +1,4 @@
+package ua.com.company.entity;
+
+public class MyIvideonEntity {
+}
